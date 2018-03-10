000953382ed1f618f5dc0538bba04eac4a732d09
webdeveloperernit <webdeveloper.ernit@gmail.com>
2018-03-10 19:54:27 +0100

    Update products list table
    - Update sorting function
    - UI updates

--- src/app/components/products/productslisttable/productslisttable.component.ts
+++ src/app/components/products/productslisttable/productslisttable.component.ts
@@ -56,34 +56,39 @@ export class ProductsListTableComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.productsListInfo.map(p => {
+      p.status = this.getStatus(p.stock, p.reorderPoint);
+    });
+  }
+
+  getStockColor(stock, reorderPoint) {
+    if (stock < reorderPoint) {
+      return 'red';
+    } else if (stock < reorderPoint + 3) {
+      return 'orange';
+    } else {
+      return 'green';
+    }
+  }
+
+  getStatus(stock, reorderPoint) {
+    if (stock === undefined) {
+      return 'Active';
+    } else {
+      if (stock < 0) {
+        return 'No Stock!';
+      } else if (stock < reorderPoint) {
+        return 'Below re-order point';
+      } else if (stock < reorderPoint + 3) {
+        return 'Place order';
+      } else {
+        return 'Active';
+      }
+    }
   }
 
   redirectTo(id) {
-    this.router.navigate(['../profile/' + id]);
-  }
-
-  selectedFilterEventHandler(filteredList) {
-  }
-
-  getScoreColor(score) {
-    return score >= 65 ? 'green' : score >= 35 ? 'orange' : 'red';
-  }
-
-  getDateColor(value) {
-    return value === 'Follow-up' ? 'orange' : value === 'Lost' ? 'orange' : 'green';
-  }
-
-  openProductModal(index) {
-    this.clickSettingCount ++;
-    // switch (this.clickSettingCount % 3) {
-    //   case 0: this.productModalInfoCollapsed[index] = false;
-    //   break;
-    //   case 1: this.productModalInfoCollapsed[index] = true;
-    //   break;
-    //   default: this.productModalInfoCollapsed[index] = true;
-    //   break;
-    // }
-    this.productModalInfoCollapsed[index] = true;
+    this.router.navigate(['../product/' + id]);
   }
 
   sortArray(field) {
@@ -104,41 +109,6 @@ export class ProductsListTableComponent implements OnInit {
     }
   }
 
-  sortRatingArray(field) {
-    const cmp = this;
-    cmp.sortScoreClicked = ! cmp.sortScoreClicked;
-    if (!cmp.sortScoreClicked) {
-      this.productsListInfo.sort( function(name1, name2) {
-        if ( Number(name1[field]) < Number(name2[field]) ) {
-          return -1;
-        } else if ( Number(name1[field]) > Number(name2[field])) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-    } else {
-      this.productsListInfo.reverse();
-    }
-  }
-
-  sortDealsArray(field) {
-    const cmp = this;
-    cmp.sortScoreClicked = ! cmp.sortScoreClicked;
-    if (!cmp.sortScoreClicked) {
-      this.productsListInfo.sort( function(name1, name2) {
-        if ( name1[field] < name2[field] ) {
-          return -1;
-        } else if ( name1[field] > name2[field]) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-    } else {
-      this.productsListInfo.reverse();
-    }
-  }
 
   // SortProductStatus() {
   //   const cmp = this;
@@ -157,99 +127,5 @@ export class ProductsListTableComponent implements OnInit {
   //   }
   // }
 
-
-  addNewTimelineItem() {
-    const date = new Date();
-    const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-
-    const nitem = {
-      title: this.activity.title,
-      content: this.activity.content,
-      icon: 'fa fa-home',
-      timelineBtnColor: 'green-btn',
-      buttontitle: 'More Info',
-      date: today,
-      buttonClickEventHandlerName: 'getMoreInfo',
-      subject: this.activity.subject,
-      product: this.activity.product
-    };
-
-    switch (nitem.title) {
-      case 'Notes':
-        nitem.icon = 'fa-file-text-o';
-        break;
-      case 'Email':
-        nitem.icon = 'fa fa-envelope-o';
-      break;
-      case 'Call':
-        nitem.icon = 'fa fa-phone';
-      break;
-      default:
-        nitem.icon = 'fa fa-home';
-        break;
-    }
-  }
-
-  cloneRow(product, index) {
-    this.clonedRowIndex = index;
-    this.clonedRowProduct = product;
-    this.productModalInfoCollapsed[index] = false;
-    this.showProductModalInfo = false;
-    this.showCloneConfirmModal = true;
-  }
-
-  deleteRow(index) {
-    this.deletedRowIndex = index;
-    this.productModalInfoCollapsed[index] = false;
-    this.showProductModalInfo = false;
-    this.showDeleteConfirmModal = true;
-  }
-
-  confirmClone() {
-    this.productsListInfo.splice(this.clonedRowIndex, 0, this.clonedRowProduct);
-  }
-
-  confirmDelete() {
-    this.productsListInfo.splice(this.deletedRowIndex, 1);
-  }
-
-  clickOutsideInfo(i) {
-    this.productModalInfoCollapsed[i] = false;
-    this.showProductModalInfo = false;
-    this.phoneClicked = false;
-    this.emailClicked = false;
-  }
-
-  phoneClick(product) {
-    this.phoneClicked = true;
-    this.emailClicked = false;
-    this.expandedInfoModal = false;
-    this.formatedPhone = this.formatPhoneNumber(product.phone);
-  }
-
-  emailClick() {
-    this.phoneClicked = false;
-    this.emailClicked = true;
-    this.expandedInfoModal = false;
-  }
-
-  mapClick(task) {}
-
-  expandProductModal(index) {
-    this.phoneClicked = false;
-    this.emailClicked = false;
-    this.clickSettingCount ++;
-    if (this.clickSettingCount % 2 === 1) {
-      this.expandedInfoModal = false;
-    } else {
-      this.expandedInfoModal = true;
-    }
-  }
-
-  formatPhoneNumber(s) {
-    const s2 = ('' + s).replace(/\D/g, '');
-    const m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
-    return (!m) ? null : '(' + m[1] + ') ' + m[2] + '-' + m[3];
-  }
 }
 
