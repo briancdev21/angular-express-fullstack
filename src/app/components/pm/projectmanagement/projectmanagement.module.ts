f6cdab5816d3fc586a391377bf03816fdb6f22f7
Latif <leadhead1987@gmail.com>
2018-05-09 17:10:48 +0200

    Migrate pm page to Ernit.

--- src/app/components/pm/projectmanagement/projectmanagement.module.ts
+++ src/app/components/pm/projectmanagement/projectmanagement.module.ts
@@ -45,6 +45,15 @@ export const MY_NATIVE_FORMATS = {
   monthYearA11yLabel: {year: 'numeric', month: 'long'},
 };
 
+export const MY_NATIVE_FORMATS = {
+  fullPickerInput: {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'},
+  datePickerInput: {year: 'numeric', month: 'numeric', day: 'numeric'},
+  timePickerInput: {hour: 'numeric', minute: 'numeric'},
+  monthYearLabel: {year: 'numeric', month: 'short'},
+  dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+  monthYearA11yLabel: {year: 'numeric', month: 'long'},
+};
+
 @NgModule({
   declarations: [
     PmBreadcrumbBarComponent,
