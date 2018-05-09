e9b307d7b20179a9d2f15ac80cccabf04a782948
Latif <leadhead1987@gmail.com>
2018-05-09 11:25:48 +0200

    Move Project management module into pm module
    - set routing
    - fix responsive issue on tab bar
    - remove sub menu bar on sales page when going into pm page.

--- src/app/components/pm/projectmanagement/pmprogress/pmprogress.component.ts
+++ src/app/components/pm/projectmanagement/pmprogress/pmprogress.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PmService } from '../pm.service';
+import { ChangeLogListTableComponent } from './changeloglisttable/changeloglisttable.component';
 
 @Component({
   selector: 'app-pmprogress',
@@ -11,22 +12,48 @@ import { PmService } from '../pm.service';
 })
 
 export class PmProgressComponent implements OnInit {
-  tabActiveFirst: boolean;
-  tabActiveSecond: boolean;
-  tabActiveThird: boolean;
-  tabActiveFourth: boolean;
+  public changelogListInfo: Array<Object> = [
+    {
+      logId: 'CL0159-01',
+      logName: 'Changing of the TV location in Living Room',
+      priority: 1,
+      requestedBy: 'Sepehr Shoarinejad',
+      budgetImpact: 2000,
+      scheduleImpact: -14,
+      dateCreated: 'November 20, 2016',
+      lastUpdated: 'January 20, 2017',
+      dateApproved: 'January 20, 2017',
+    },
+    {
+      logId: 'CL0159-02',
+      logName: 'Customer constantly changing his mind',
+      priority: 1,
+      requestedBy: 'Sepehr Shoarinejad',
+      budgetImpact: -2000,
+      scheduleImpact: 2,
+      dateCreated: 'December 15, 2016',
+      lastUpdated: 'March 20, 2017',
+      dateApproved: 'April 20, 2017',
+    },
+    {
+      logId: 'CL0159-03',
+      logName: 'I don\'t know what to do anymore',
+      priority: 1,
+      requestedBy: 'Tyler Labonte',
+      budgetImpact: 3500,
+      scheduleImpact: 4,
+      dateCreated: 'January 20, 2016',
+      lastUpdated: 'June 20, 2017',
+      dateApproved: 'June 20, 2017',
+    },
+  ];
 
-  constructor( private pmService: PmService, private router: Router ) {
+
+  constructor( private pmService: PmService ) {
   }
 
   ngOnInit() {
 
-  }
-
-  tabChange(event) {
-    if (event.tabTitle === 'Change Log') {
-      this.router.navigate(['./pm/pm-details/pm-progress/pm-logs-table/']);
-    }
   }
 
 }
--- src/app/components/pm/projectmanagement/projectmanagement.module.ts
+++ src/app/components/pm/projectmanagement/projectmanagement.module.ts
@@ -2,55 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { NgSelectModule } from '@ng-select/ng-select';
+import { AutocompleteModule } from 'ng2-input-autocomplete';
 import { PmBreadcrumbBarComponent } from './pmbreadcrumbbar/pmbreadcrumbbar.component';
 import { PmBoardComponent } from './pmboard/pmboard.component';
 import { PmScheduleComponent } from './pmschedule/pmschedule.component';
 import { PmFinancialsComponent } from './pmfinancials/pmfinancials.component';
 import { PmProgressComponent } from './pmprogress/pmprogress.component';
 import { PmFilesComponent } from './pmfiles/pmfiles.component';
-import { PmBoardTopInfoComponent } from './pmboard/pmboardtopinfo/pmboardtopinfo.component';
-import { BoardPanelRightComponent } from './pmboard/boardpanelright/boardpanelright.component';
-import { ProjectFinanceComponent } from './pmboard/projectfinance/projectfinance.component';
-import { PmBoardTableComponent } from './pmboard/pmboardtable/pmboardtable.component';
-import { AddDetailedTaskComponent } from '../adddetailedtask/adddetailedtask.component';
-import { SubTasksManagementComponent } from '../adddetailedtask/subtasksmanagement/subtasksmanagement.component';
-import { GanttChartComponent } from './pmboard/ganttchart/ganttchart.component';
-import { PmTasksTableComponent } from './pmschedule/pmtaskstable/pmtaskstable.component';
-import { ChangeLogProfileComponent } from './pmprogress/progresschangelog/changelogprofile/changelogprofile.component';
-import { ChangeLogDetailsComponent } from './pmprogress/progresschangelog/changelogdetails/changelogdetails.component';
-import { ChangeLogListTableComponent } from './pmprogress/progresschangelog/changeloglisttable/changeloglisttable.component';
-import { ChangeLogSettingsComponent } from './pmprogress/progresschangelog/changelogsettings/changelogsettings.component';
-import { ChangeLogsTableComponent } from './pmprogress/progresschangelog/changelogstable/changelogstable.component';
-import { ProgressOverviewComponent } from './pmprogress/progressoverview/progressoverview.component';
-import {
-    ProgressProjectInformationComponent
-  } from './pmprogress/progressoverview/progressprojectinformation/progressprojectinformation.component';
-import { ProgressProjectBriefComponent } from './pmprogress/progressprojectbrief/progressprojectbrief.component';
-
+import { ChangeLogListTableComponent } from './pmprogress/changeloglisttable/changeloglisttable.component';
 import { CommonCmpModule } from '../../../components/common/common.module';
-import { ProfileCmpModule } from '../../profile/profile.module';
 import { BreadcrumbModule } from '../../../components/breadcrumb/breadcrumb.module';
-import { ProgressProductLogCmpModule } from './pmprogress/progressproductlog/progressproductlog.module';
-import { ClickOutsideModule } from 'ng4-click-outside';
-import { DragulaModule } from 'ng2-dragula';
 
 import { TabModule } from 'angular-tabs-component';
 import { FormsModule } from '@angular/forms';
-import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_FORMATS } from 'ng-pick-datetime';
-import { QuillModule } from 'ngx-quill';
 
 import { PmService } from './pm.service';
-import { TaskStableMapToKeysPipe } from './pmschedule/pmtaskstable/map-to-keys.pipe';
-
-export const MY_NATIVE_FORMATS = {
-  fullPickerInput: {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'},
-  datePickerInput: {year: 'numeric', month: 'numeric', day: 'numeric'},
-  timePickerInput: {hour: 'numeric', minute: 'numeric'},
-  monthYearLabel: {year: 'numeric', month: 'short'},
-  dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
-  monthYearA11yLabel: {year: 'numeric', month: 'long'},
-};
 
 @NgModule({
   declarations: [
@@ -60,39 +26,16 @@ export const MY_NATIVE_FORMATS = {
     PmFinancialsComponent,
     PmProgressComponent,
     PmFilesComponent,
-    PmBoardTopInfoComponent,
-    BoardPanelRightComponent,
-    ProjectFinanceComponent,
-    PmBoardTableComponent,
-    AddDetailedTaskComponent,
-    SubTasksManagementComponent,
-    GanttChartComponent,
-    PmTasksTableComponent,
-    ChangeLogProfileComponent,
-    ChangeLogDetailsComponent,
-    ChangeLogListTableComponent,
-    ChangeLogSettingsComponent,
-    ChangeLogsTableComponent,
-    TaskStableMapToKeysPipe,
-    ProgressOverviewComponent,
-    ProgressProjectInformationComponent,
-    ProgressProjectBriefComponent
+    ChangeLogListTableComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     BreadcrumbModule,
     CommonCmpModule,
-    ProfileCmpModule,
     TabModule,
-    NgSelectModule,
+    AutocompleteModule,
     FormsModule,
-    OwlDateTimeModule,
-    OwlNativeDateTimeModule,
-    ClickOutsideModule,
-    DragulaModule,
-    QuillModule,
-    ProgressProductLogCmpModule
   ],
   exports: [
     PmBreadcrumbBarComponent,
@@ -101,14 +44,8 @@ export const MY_NATIVE_FORMATS = {
     PmFinancialsComponent,
     PmProgressComponent,
     PmFilesComponent,
-    AddDetailedTaskComponent,
-    SubTasksManagementComponent,
-    GanttChartComponent,
-    TaskStableMapToKeysPipe
+    ChangeLogListTableComponent
   ],
-  providers: [
-    PmService,
-    {provide: OWL_DATE_TIME_FORMATS, useValue: MY_NATIVE_FORMATS},
-  ]
+  providers: [PmService]
 })
 export class ProjectManagementCmpModule { }
