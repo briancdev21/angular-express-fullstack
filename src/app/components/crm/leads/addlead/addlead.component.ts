d989528c894d809e0085d6598273b222767363e4
webdeveloperernit <webdeveloper.ernit@gmail.com>
2018-05-10 04:41:48 +0200

    Update add lead function

--- src/app/components/crm/leads/addlead/addlead.component.ts
+++ src/app/components/crm/leads/addlead/addlead.component.ts
@@ -76,7 +76,7 @@ export class AddLeadComponent implements OnInit {
   Test2: any;
   currencyList = [];
   keywordsIdList = [];
-  headContact = '';
+  headContact: any;
   jobTitle = '';
   email = '';
   notes = '';
@@ -92,6 +92,9 @@ export class AddLeadComponent implements OnInit {
   usersList = [];
   sourcesList = [];
   sourcesNameList = [];
+  selectedSourceId: any;
+  businessAssociation: any;
+  newLead: any;
 
   constructor(private completerService: CompleterService, private sharedService: SharedService, private crmService: CrmService) {
     this.dataService = completerService.local(this.searchData, 'color', 'color');
@@ -102,7 +105,6 @@ export class AddLeadComponent implements OnInit {
   ngOnInit() {
     this.sharedService.getCurrencies().subscribe(data => {
       this.currencyList = data.results;
-      console.log('currency List: ', data.results);
     });
 
     this.sharedService.getTerms().subscribe(res => {
@@ -114,18 +116,16 @@ export class AddLeadComponent implements OnInit {
     });
 
     this.sharedService.getContacts().subscribe(res => {
-      this.contactsList = res.results;
-      console.log('contacts: ', res);
+      this.contactsList = res;
     });
 
     this.sharedService.getUsers().subscribe(res => {
-      this.usersList = res.results;
-      console.log('users: ', res);
+      this.usersList = res;
     });
 
     this.sharedService.getSources().subscribe(res => {
       this.sourcesList = res.results;
-      console.log('sources: ', res.results);
+      this.sourcesNameList = res.results.map(n => n.source);
     });
   }
 
@@ -156,10 +156,11 @@ export class AddLeadComponent implements OnInit {
     } else {
       this.sourceValue = true;
     }
+    const pos = this.sourcesNameList.indexOf(event.title);
+    this.selectedSourceId = this.sourcesList[pos].id;
   }
 
   getKeywords(event) {
-    console.log('keywords: ', event);
     this.keywordsIdList = event.map(k => k.id);
   }
 
@@ -287,59 +288,107 @@ export class AddLeadComponent implements OnInit {
     this.invalidDefaultCurrency = false;
     this.invalidDefaultPricing = false;
     if (this.defaultTerm && this.defaultCurrency && this.defaultPricing) {
-      const newLead = {
-        'currencyId': parseInt(this.defaultCurrency, 10),
-        'termId': parseInt(this.defaultTerm, 10),
-        'sourceId': 1,
-        'pricingCategoryId': parseInt(this.defaultPricing, 10),
-        'keywordIds': this.keywordsIdList,
-        'owner': 'string',
-        'followers': [
-          'string'
-        ],
-        'type': this.businessType,
-        'person': {
-          'firstName': this.firstName,
-          'lastName': this.lastName,
-          'jobTitle': this.jobTitle,
-          'department': this.captain,
-          'businessAssociation': 1
-        },
-        'business': {
-          'name': this.businessName,
-          'headContact': 1,
-          'accountReceivable': 1,
-          'personAssociations': [
-            0
-          ]
-        },
-        'shippingAddress': {
-          'address': this.address,
-          'city': this.city,
-          'province': this.province,
-          'postalCode': this.postalCode,
-          'country': this.country
-        },
-        'billingAddress': {
-          'address': this.billingAddress,
-          'city': this.billingCity,
-          'province': this.billingProvince,
-          'postalCode': this.billingPostalCode,
-          'country': this.billingCountry
-        },
-        'email':  this.email,
-        'socialMediaUrl': {
-          'linkedIn': 'string',
-          'facebook': 'string',
-          'twitter': 'string'
-        },
-        'phoneNumbers': {
-          'primary': this.primaryNumber,
-          'secondary': this.secondaryNumber,
-        },
-        'timezone': 1,
-        'note': this.notes,
-        'lastContacted': moment().format('YYYY-MM-DD')
+      if (this.businessType === 'PERSON') {
+        this.newLead = {
+          'currencyId': parseInt(this.defaultCurrency, 10),
+          'termId': parseInt(this.defaultTerm, 10),
+          'sourceId': parseInt(this.selectedSourceId, 10),
+          'pricingCategoryId': parseInt(this.defaultPricing, 10),
+          'keywordIds': this.keywordsIdList,
+          'owner': 'string',
+          'followers': [
+            'string'
+          ],
+          'type': this.businessType,
+          'person': {
+            'firstName': this.firstName,
+            'lastName': this.lastName,
+            'jobTitle': this.jobTitle,
+            'department': this.captain,
+            'businessAssociation': parseInt(this.businessAssociation, 10),
+          },
+          'shippingAddress': {
+            'address': this.address,
+            'city': this.city,
+            'province': this.province,
+            'postalCode': this.postalCode,
+            'country': this.country
+          },
+          'billingAddress': {
+            'address': this.billingAddress,
+            'city': this.billingCity,
+            'province': this.billingProvince,
+            'postalCode': this.billingPostalCode,
+            'country': this.billingCountry
+          },
+          'email':  this.email,
+          'socialMediaUrl': {
+            'linkedIn': 'string',
+            'facebook': 'string',
+            'twitter': 'string'
+          },
+          'phoneNumbers': {
+            'primary': this.primaryNumber,
+            'secondary': this.secondaryNumber,
+          },
+          'timezone': 1,
+          'note': this.notes,
+          'lastContacted': moment().format('YYYY-MM-DD')
+        };
+      } else {
+        this.newLead = {
+          'currencyId': parseInt(this.defaultCurrency, 10),
+          'termId': parseInt(this.defaultTerm, 10),
+          'sourceId': parseInt(this.selectedSourceId, 10),
+          'pricingCategoryId': parseInt(this.defaultPricing, 10),
+          'keywordIds': this.keywordsIdList,
+          'owner': 'string',
+          'followers': [
+            'string'
+          ],
+          'type': this.businessType,
+          'person': {
+            'jobTitle': this.jobTitle,
+            'department': this.captain,
+            'businessAssociation': parseInt(this.businessAssociation, 10),
+          },
+          'business': {
+            'name': this.businessName,
+            'headContact': parseInt(this.headContact, 10),
+            'accountReceivable': 1,
+            'personAssociations': [
+              1
+            ]
+          },
+          'shippingAddress': {
+            'address': this.address,
+            'city': this.city,
+            'province': this.province,
+            'postalCode': this.postalCode,
+            'country': this.country
+          },
+          'billingAddress': {
+            'address': this.billingAddress,
+            'city': this.billingCity,
+            'province': this.billingProvince,
+            'postalCode': this.billingPostalCode,
+            'country': this.billingCountry
+          },
+          'email':  this.email,
+          'socialMediaUrl': {
+            'linkedIn': 'string',
+            'facebook': 'string',
+            'twitter': 'string'
+          },
+          'phoneNumbers': {
+            'primary': this.primaryNumber,
+            'secondary': this.secondaryNumber,
+          },
+          'timezone': 1,
+          'note': this.notes,
+          'lastContacted': moment().format('YYYY-MM-DD')
+        };
+      }
           // "currencyId": 1,
           // "termId": 1,
           // "sourceId": 1,
@@ -394,9 +443,7 @@ export class AddLeadComponent implements OnInit {
           // "timezone": 1,
           // "note": "string",
           // "lastContacted": "2018-05-08"
-      };
-      this.crmService.createLead(JSON.stringify(newLead)).subscribe(data => {
-        console.log('data: ', data);
+      this.crmService.createLead(JSON.stringify(this.newLead)).subscribe(data => {
         this.addLeadModalCollapsed = true;
         this.showAddLeadModal = false;
         this.tabActiveFirst = true;
