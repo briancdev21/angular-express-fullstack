5d4c13b97257e61f4056f6356a36d56194a6c53d
Dennis <softdev.d.r@gmail.com>
2018-05-11 17:51:55 -0300

    integrate Address component

--- src/app/components/sales/invoices/components/shippingaddress/shippingaddress.component.ts
+++ src/app/components/sales/invoices/components/shippingaddress/shippingaddress.component.ts
@@ -6,20 +6,25 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./shippingaddress.component.css']
 })
 export class ShippingAddressComponent {
-  @Input() customerAddress;
+  @Input() set customerAddress(contactUserAddress: any) {
+    this._customerAddress = contactUserAddress;
+    console.log('_customerAddress', this._customerAddress);
+  }
   @Input() shippingAddress;
+
+  _customerAddress: any = {};
 
   originshippingaddress = {
     address: '',
-    street: '',
     city: '',
+    province: '',
     country: '',
-    postcode: ''
+    postalCode: ''
   };
 
   onSwitchChanged(status: boolean) {
     if (status) {
-      this.shippingAddress = this.customerAddress;
+      this.shippingAddress = this._customerAddress;
     } else {
       this.shippingAddress = this.originshippingaddress;
     }
