f1791410025cae235444253803d100192c91b117
Dennis <softdev.d.r@gmail.com>
2018-05-11 15:32:12 -0300

    Update Invoices service

--- src/app/services/invoices.service.ts
+++ src/app/services/invoices.service.ts
@@ -2,6 +2,14 @@ import { Injectable, Output, Input, } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+import { apiUrl } from '../config';
+import { map, tap } from 'rxjs/operators';
+
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 
@@ -16,8 +24,53 @@ export class InvoicesService {
   deleteOpenedTask = this.deleteTask.asObservable();
   ganttUpdated = this.updateChart.asObservable();
 
-  constructor() {
+  constructor( private http: HttpClient ) {
 
+  }
+
+  getInvoices (): Observable<any> {
+    const url = `${apiUrl}sales/invoices`;
+    return this.http.get(url);
+  }
+
+  createInvoice (body): Observable<any> {
+    const url = `${apiUrl}sales/invoices`;
+    return this.http.post<any>(url, body);
+  }
+
+  getIndividualInvoice(id): Observable<any>  {
+    const url = `${apiUrl}sales/invoices/${id}`;
+    return this.http.get(url);
+  }
+
+  updateInvoice (id, body): Observable<any> {
+    const url = `${apiUrl}sales/invoices/${id}`;
+    return this.http.put<any>(url, body);
+  }
+
+  getInvoicesProducts (id): Observable<any> {
+    const url = `${apiUrl}sales/invoices/${id}/products`;
+    return this.http.get(url);
+  }
+
+  createInvoiceProduct (id, body): Observable<any> {
+    const url = `${apiUrl}sales/invoices/${id}/products`;
+    return this.http.post<any>(url, body);
+  }
+
+  getIndividualInvoiceProducts(id, productId): Observable<any>  {
+    const url = `${apiUrl}sale/invoices/${id}/products/${productId}`;
+    return this.http.get(url);
+  }
+
+  updateInvoiceProduct (id, productId, body): Observable<any> {
+    const url = `${apiUrl}sales/invoices/${id}/products/${productId}`;
+    return this.http.put<any>(url, body);
+  }
+
+  deleteInvoiceProduct (id, productId, body): Observable<any> {
+    const url = `${apiUrl}sales/invoices/${id}/products/${productId}`;
+    return this.http.delete<any>(url, body);
   }
 
   hideDetailedTaskModal(data) {
