f91dab195e721813417d50890eb6ad9e0e83969e
webdeveloperernit <webdeveloper.ernit@gmail.com>
2018-06-15 17:56:24 +0200

    Add variant keywords component.

---
+++ src/app/components/inventory/products/productprofile/editproductmodal/variantmultikeyword/variantmultikeyword.component.ts
@@ -0,0 +1,64 @@
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, Renderer, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+  selector: 'app-variantmultikeyword',
+  templateUrl: './variantmultikeyword.component.html',
+  styleUrls: [
+    './variantmultikeyword.component.css'
+  ]
+})
+
+export class VariantMultiKeywordComponent implements AfterViewInit, OnInit {
+  @Input() keywords;
+  @ViewChild('box') input: ElementRef;
+  @Input() placeholder;
+  @Output() sendKeywords: EventEmitter<any> = new EventEmitter;
+  editable: boolean;
+  newKeyword: string;
+  keywordsNameList = [];
+  keywordsList = [];
+
+  constructor(private renderer: Renderer) {
+    const comp = this;
+    document.addEventListener('click', function() {
+      comp.editable = false;
+    });
+  }
+
+  ngOnInit() {
+    const arr = [];
+    this.editable = false;
+    if (this.keywords) {
+      this.keywords.forEach(element => {
+        for (let i = 0; i < this.keywordsList.length; i ++) {
+          if (element === this.keywordsList[i].id) {
+            arr.push(this.keywordsList[i]);
+          }
+        }
+      });
+    }
+
+    this.keywords = arr;
+  }
+
+  ngAfterViewInit() {
+    // this.input.nativeElement.focus();
+  }
+
+  addNewKeyword(data) {
+    this.newKeyword = '';
+    if (!this.keywords.includes(data)) {
+      this.keywords.push(data);
+      this.sendKeywords.emit(this.keywords);
+    }
+  }
+
+  deleteKeyword(data) {
+    const pos = this.keywords.indexOf(data);
+    this.keywords.splice(pos, 1);
+    this.sendKeywords.emit(this.keywords);
+  }
+
+}
