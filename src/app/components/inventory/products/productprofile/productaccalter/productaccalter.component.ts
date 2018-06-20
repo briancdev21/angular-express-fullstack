c121910d217ceab93b1dfeca27450fede69351a1
webdeveloperernit <webdeveloper.ernit@gmail.com>
2018-06-20 05:11:31 +0200

    Update product Alter / Acc component

--- src/app/components/inventory/products/productprofile/productaccalter/productaccalter.component.ts
+++ src/app/components/inventory/products/productprofile/productaccalter/productaccalter.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { SharedService } from '../../../../../services/shared.service';
 import { ProductsService } from '../../../../../services/inventory/products.service';
+import { ProductProfileService } from '../productprofile.service';
 
 @Component({
   selector: 'app-productaccalter',
@@ -20,7 +21,8 @@ export class ProductAccAlterComponent {
   pricingCategoriesListInfo = [];
   productsList = [];
   brandsList = [];
-  constructor( private sharedService: SharedService, private productsService: ProductsService, private route: ActivatedRoute) {
+  constructor( private sharedService: SharedService, private productsService: ProductsService, private route: ActivatedRoute,
+    private productProfileService: ProductProfileService) {
     this.sharedService.getBrands().subscribe(res => {
       this.brandsList = res.results;
     });
@@ -37,6 +39,19 @@ export class ProductAccAlterComponent {
         this.productAccessories = res.results;
         console.log('acce: ', res);
       });
+    });
+
+    this.productProfileService.editModalClosed.subscribe(data => {
+      if (data) {
+        this.productsService.getProductAlternativesList(this.productInfoIndex).subscribe(res => {
+          this.productAlternatives = res.results;
+          console.log('alternatives: ', res);
+        });
+        this.productsService.getProductAccessoriesList(this.productInfoIndex).subscribe(res => {
+          this.productAccessories = res.results;
+          console.log('acce: ', res);
+        });
+      }
     });
   }
 
