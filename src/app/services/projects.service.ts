8f76a124d2fcdcbdd24810615be09cda17a83223
webdeveloperernit <webdeveloper.ernit@gmail.com>
2018-06-22 13:50:20 +0200

    Integrate Pending financials page

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -33,6 +33,22 @@ export class ProjectsService {
     return this.http.get(url);
   }
 
+  getProjectCostSummary (id): Observable<any> {
+    const url = `${environment.apiUrl}/project-management/projects/${id}/cost-summaries`;
+    return this.http.get(url);
+  }
+
+  createProjectCostSummary (id, body): Observable<any> {
+    const url = `${environment.apiUrl}/project-management/projects/${id}/cost-summaries`;
+    return this.http.post(url, body, this.options)
+      .map((res) => res);
+  }
+
+  deleteIndividualProjectCostSummary(id, summaryId): Observable<any>  {
+    const url = `${environment.apiUrl}/project-management/projects/${id}/cost-summaries/${summaryId}`;
+    return this.http.delete(url);
+  }
+
   // createSupplier (body): Observable<any> {
   //   // const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }), method: 'POST' };
   //   const url = `${apiUrl}inventory/suppliers/`;
