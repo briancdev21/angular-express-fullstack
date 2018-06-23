c02bad5841c7db567363e62f479d4b5401160ba7
Dennis <softdev.d.r@gmail.com>
2018-06-23 04:48:00 -0300

    Integrate order details part

--- src/app/components/collaboration/workorders/orderprofile/orderdetails/orderdetails.component.ts
+++ src/app/components/collaboration/workorders/orderprofile/orderdetails/orderdetails.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { OrderService } from '../order.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-orderdetails',
@@ -13,8 +14,8 @@ export class OrderDetailsComponent implements OnInit {
 
   @Input() orderProfileInfo;
   switchIconShipping = false;
-  street = '';
-  city = '';
+  street: any;
+  city: any;
   state = '';
   country = '';
   zipcode = '';
@@ -22,31 +23,43 @@ export class OrderDetailsComponent implements OnInit {
   public startMax;
   startDateValue: Date;
   endDateValue: Date;
-  startTimeValue: Date;
-  endTimeValue: Date;
+  startTimeValue: any;
+  endTimeValue: any;
 
   constructor( private orderService: OrderService ) {
 
   }
 
   ngOnInit () {
-    var options = { month: 'long', year: 'numeric', day: 'numeric' };
+    const options = { month: 'long', year: 'numeric', day: 'numeric' };
 
     // const midVal = new Intl.DateTimeFormat('en-US', dateA11yLabel).format(this.orderProfileInfo.startDate);
     // this.startDateValue = new Date(midVal);
+    console.log('order info: ', this.orderProfileInfo);
     this.startDateValue = new Date(this.orderProfileInfo.startDate);
     this.endDateValue = new Date(this.orderProfileInfo.endDate);
-    this.startTimeValue = new Date(this.orderProfileInfo.startTime);
-    this.endTimeValue = new Date(this.orderProfileInfo.startEnd);
+    this.startTimeValue = new Date (moment(this.orderProfileInfo.startDate).format('hh:mm:ss a'));
+    this.endTimeValue = moment(this.orderProfileInfo.endDate).format('hh:mm:ss a');
+    this.street = this.orderProfileInfo.shippingAddress.address;
+    this.city = this.orderProfileInfo.shippingAddress.city;
+    this.state = this.orderProfileInfo.shippingAddress.province;
+    this.country = this.orderProfileInfo.shippingAddress.country;
+    this.zipcode = this.orderProfileInfo.shippingAddress.postalCode;
   }
 
   clickIconShipping() {
+
+    this.orderProfileInfo.shippingAddress.address = (this.switchIconShipping) ? this.street :
+                                                                                this.orderProfileInfo.selectedContact.shippingAddress.city;
+    this.orderProfileInfo.shippingAddress.city = (this.switchIconShipping) ? this.city :
+                                            this.orderProfileInfo.selectedContact.shippingAddress.city;
+    this.orderProfileInfo.shippingAddress.province = (this.switchIconShipping) ? this.state :
+                                              this.orderProfileInfo.selectedContact.shippingAddress.province;
+    this.orderProfileInfo.shippingAddress.country = (this.switchIconShipping) ? this.country :
+                                              this.orderProfileInfo.selectedContact.shippingAddress.country;
+    this.orderProfileInfo.shippingAddress.postalCode = (this.switchIconShipping) ? this.zipcode :
+                                              this.orderProfileInfo.selectedContact.shippingAddress.postalCode;
     this.switchIconShipping = !this.switchIconShipping;
-    this.street = (this.switchIconShipping) ? this.orderProfileInfo.shippingAddress.street : '';
-    this.city = (this.switchIconShipping) ? this.orderProfileInfo.shippingAddress.city : '';
-    this.state = (this.switchIconShipping) ? this.orderProfileInfo.shippingAddress.state : '';
-    this.country = (this.switchIconShipping) ? this.orderProfileInfo.shippingAddress.country : '';
-    this.zipcode = (this.switchIconShipping) ? this.orderProfileInfo.shippingAddress.zipcode : '';
   }
 
   onStartDate(event) {
@@ -70,5 +83,5 @@ export class OrderDetailsComponent implements OnInit {
     this.endTimeValue = event.value;
     this.orderService.postTimelineData({title: this.endTimeValue.toTimeString(), type: 'endTime'});
   }
-  
+
 }
