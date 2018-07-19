a415af81f0b9a84eada6ac7aeab72b23cf2d9a3c
Dennis <softdev.d.r@gmail.com>
2018-07-19 04:43:26 -0300

    Comment unneccessary data at proposal component

--- src/app/components/sales/proposal/proposal.component.ts
+++ src/app/components/sales/proposal/proposal.component.ts
@@ -16,20 +16,7 @@ import * as moment from 'moment';
 })
 export class ProposalComponent implements OnInit {
 
-  constructor( private proposalService: ProposalService, private route: ActivatedRoute, private sharedService: SharedService,
-    private proposalsService: ProposalsService ) {
-    this.proposalId = this.route.snapshot.paramMap.get('id');
-    this.sharedService.getProjectTypes().subscribe(res => {
-      this.productTypesList = res;
-    });
-    this.sharedService.getUsers().subscribe(res => {
-      this.userList = res;
-    });
 
-    this.getProposalData();
-
-
-  }
   menuCollapsed = true;
   proposalId: any;
   public searchQueryString = '';
@@ -64,6 +51,19 @@ export class ProposalComponent implements OnInit {
 
   public projectDetails: any;
 
+  constructor( private proposalService: ProposalService, private route: ActivatedRoute, private sharedService: SharedService,
+    private proposalsService: ProposalsService ) {
+    this.proposalId = this.route.snapshot.paramMap.get('id');
+    this.sharedService.getProjectTypes().subscribe(res => {
+      this.productTypesList = res;
+    });
+    this.sharedService.getUsers().subscribe(res => {
+      this.userList = res;
+    });
+
+    this.getProposalData();
+  }
+
   ngOnInit() {
     this.proposalService.getUpdatedProposalProductList.subscribe(
       data => {
@@ -81,7 +81,8 @@ export class ProposalComponent implements OnInit {
       this.contactsList = data;
       this.proposalsService.getIndividualProposal(this.proposalId).subscribe(res => {
         console.log('proposals service: ', res);
-        this.getCategoryDetails(res.data.categoryIds);
+        // this.getCategoryDetails(res.data.categoryIds);
+        // this.getSubCategoryDetails(res.data.subcategoryIds);
         this.proposalDetails = res.data;
         this.proposalInfo = {
           proposalId : res.data.id,
@@ -199,38 +200,38 @@ export class ProposalComponent implements OnInit {
   }
 
   getProjectDetails() {
-    const projectDetails = {
-      proposalId: this.proposalDetails['id'],
-      discount: {
-        amount: this.proposalDetails['discount'].value,
-        type: this.proposalDetails['discount'].unit === 'AMOUNT' ? ['$'] : ['%']
-      },
-      projectId: this.proposalDetails['projectId'],
-      projectCategoriesAll: this.categoryDetails,
-      projectSubCategoriesAll: this.subCategoryDetails,
-      completionDate: this.proposalDetails['completionDate'],
-      paymentSchedule: this.proposalDetails['paymentSchedule'],
-      collaborators: this.getColloboratorDetails(this.proposalDetails['collaborators']),
-      accountManager: this.getContactInfo(this.proposalDetails['accountManager']),
-      projectManager: this.getContactInfo(this.proposalDetails['projectManager']),
-      designer: this.getContactInfo(this.proposalDetails['designer']),
-      internalNote: this.proposalDetails['internalNote'],
-      clientNote: this.proposalDetails['clientNote'],
-      projectName: this.proposalDetails['projectName'],
-      scopeOfWork: this.proposalDetails['scopeOfWork'],
-      shippingAddress: this.proposalDetails['shippingAddress'],
-      taxRateId: this.proposalDetails['taxRateId'],
-      pricingCategoryId: this.proposalDetails['pricingCategoryId'],
-      projectTypeId: this.proposalDetails['projectTypeId'],
-      contactId: this.proposalDetails['contactId'],
-      currencyId:  this.proposalDetails['currencyId'],
-      leadId:  this.proposalDetails['leadId'],
-      clientProjectManagerId:  this.proposalDetails['clientProjectManagerId'],
-      accountReceivableId:  this.proposalDetails['accountReceivableId'],
-      name:  this.proposalDetails['name'],
-    };
-    // console.log('project details:', projectDetails);
-    this.projectDetails = projectDetails;
+    // const projectDetails = {
+    //   proposalId: this.proposalDetails['id'],
+    //   discount: {
+    //     amount: this.proposalDetails['discount'].value,
+    //     type: this.proposalDetails['discount'].unit === 'AMOUNT' ? ['$'] : ['%']
+    //   },
+    //   projectId: this.proposalDetails['projectId'],
+    //   projectCategoriesAll: this.categoryDetails,
+    //   projectSubCategoriesAll: this.subCategoryDetails,
+    //   completionDate: this.proposalDetails['completionDate'],
+    //   paymentSchedule: this.proposalDetails['paymentSchedule'],
+    //   collaborators: this.getColloboratorDetails(this.proposalDetails['collaborators']),
+    //   accountManager: this.getContactInfo(this.proposalDetails['accountManager']),
+    //   projectManager: this.getContactInfo(this.proposalDetails['projectManager']),
+    //   designer: this.getContactInfo(this.proposalDetails['designer']),
+    //   internalNote: this.proposalDetails['internalNote'],
+    //   clientNote: this.proposalDetails['clientNote'],
+    //   projectName: this.proposalDetails['projectName'],
+    //   scopeOfWork: this.proposalDetails['scopeOfWork'],
+    //   shippingAddress: this.proposalDetails['shippingAddress'],
+    //   taxRateId: this.proposalDetails['taxRateId'],
+    //   pricingCategoryId: this.proposalDetails['pricingCategoryId'],
+    //   projectTypeId: this.proposalDetails['projectTypeId'],
+    //   contactId: this.proposalDetails['contactId'],
+    //   currencyId:  this.proposalDetails['currencyId'],
+    //   leadId:  this.proposalDetails['leadId'],
+    //   clientProjectManagerId:  this.proposalDetails['clientProjectManagerId'],
+    //   accountReceivableId:  this.proposalDetails['accountReceivableId'],
+    //   name:  this.proposalDetails['name'],
+    // };
+    // // console.log('project details:', projectDetails);
+    // this.projectDetails = projectDetails;
   }
 
   addContactName(data) {
