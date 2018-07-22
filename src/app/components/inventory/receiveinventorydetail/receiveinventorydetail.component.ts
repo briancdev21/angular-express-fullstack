e52d05e0b9b76521c921f4f6424cbe5cd79b3bdc
Omaro Taylor <smart.omarodev@gmail.com>
2018-07-22 10:41:56 -0500

    Update search receive inventory

--- src/app/components/inventory/receiveinventorydetail/receiveinventorydetail.component.ts
+++ src/app/components/inventory/receiveinventorydetail/receiveinventorydetail.component.ts
@@ -20,7 +20,105 @@ export class ReceiveInventoryDetailComponent implements OnInit {
 
   public purchaseOrdersInfo = [];
 
+
+  public collaborators = {
+    projectManager: ['Michael Yue'],
+    teamMembers: ['Sepher Shoarinejad', 'Tyler Labonte'],
+    serviceTeam: [],
+    otherMembers: [],
+    collInfo: [{
+      imageUrl: 'assets/users/user1.png',
+      profileLink: 'crm/contacts/Tyler',
+      name: 'Tyler'
+    },
+    {
+      imageUrl: 'assets/users/user3.png',
+      profileLink: 'crm/contacts/michael',
+      name: 'Michael'
+    },
+    {
+      imageUrl: 'assets/users/user2.png',
+      profileLink: 'crm/contacts/joseph',
+      name: 'Joseph'
+    }]
+  };
+
+  public documents: Array<Object> = [
+    {
+      documentName: 'Nu Life Proposal',
+      documentDate: 'January 2, 2017'
+    }
+  ];
+  public upcomingModal: object = {
+    week: 'WEDNESDAY',
+    date: 'NOVEMBER 1, 2017',
+    start: '9:30 AM',
+    end: '11:00 AM',
+    duration: '1 hr, 30 min'
+  };
+
+  activity: {
+    title: string;
+    subject: string;
+    contact: string;
+    content: string;
+  };
+
+
   ngOnInit() {
+    this.activity = {
+      title: 'Notes',
+      subject: undefined,
+      contact: undefined,
+      content: ''
+    };
+  }
+
+  tabChanged(event) {
+    this.activity.title = event.tabTitle;
+  }
+
+  addNewTimelineItem() {
+    const date = new Date();
+    const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+
+    const nitem = {
+      title: this.activity.title,
+      content: this.activity.content,
+      icon: 'fa fa-home',
+      timelineBtnColor: 'green-btn',
+      buttontitle: 'More Info',
+      date: today,
+      buttonClickEventHandlerName: 'getMoreInfo',
+      subject: this.activity.subject,
+      contact: this.activity.contact
+    };
+
+    switch (nitem.title) {
+      case 'Notes':
+        nitem.icon = 'fa-file-text-o';
+        nitem.timelineBtnColor = 'lime-btn';
+        break;
+      case 'Email':
+        nitem.icon = 'fa fa-envelope-o';
+        nitem.timelineBtnColor = 'orangered-btn';
+      break;
+      case 'Call':
+        nitem.icon = 'fa fa-phone';
+        nitem.timelineBtnColor = 'orange-btn';
+      break;
+      default:
+        nitem.icon = 'fa fa-home';
+        break;
+    }
+    this.timelineData.unshift(nitem);
+    this.activity.subject = undefined;
+    this.activity.contact = undefined;
+    this.activity.title = undefined;
+  }
+
+  toggleMenubar(data: boolean) {
+    this.menuCollapsed  = data;
   }
 
 }
