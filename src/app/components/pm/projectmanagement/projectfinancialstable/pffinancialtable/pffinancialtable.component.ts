1dc15f448793095194ab6ad82be68bcac4ad023d
Latif <leadhead1987@gmail.com>
2018-07-29 15:07:00 +0200

    Update pm financials page table

--- src/app/components/pm/projectmanagement/projectfinancialstable/pffinancialtable/pffinancialtable.component.ts
+++ src/app/components/pm/projectmanagement/projectfinancialstable/pffinancialtable/pffinancialtable.component.ts
@@ -21,6 +21,7 @@ export class PfFinancialTableComponent implements OnInit {
   @Input() invoicesList;
   @Input() workOrdersList;
   @Input() tableDataAll;
+  sortClicked = true;
 
   constructor( private filterService: FilterService,  private sharedService: SharedService ) {
 
@@ -31,6 +32,54 @@ export class PfFinancialTableComponent implements OnInit {
 
   getFormatedDate(date) {
     return moment(date).format('MMMM DD, YYYY');
+  }
+
+  sortArray(field) {
+    const cmp = this;
+    cmp.sortClicked = ! cmp.sortClicked;
+    if (!cmp.sortClicked) {
+      this.tableDataAll.sort( function(name1, name2) {
+        if ( Math.abs(name1[field]) < Math.abs(name2[field])) {
+          return -1;
+        } else if ( Math.abs(name1[field]) > Math.abs(name2[field])) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    } else {
+      this.tableDataAll.reverse();
+    }
+  }
+
+  sortArrayWithString(field) {
+    const cmp = this;
+    cmp.sortClicked = ! cmp.sortClicked;
+    if (!cmp.sortClicked) {
+      this.tableDataAll.sort( function(name1, name2) {
+        return name1[field].localeCompare(name2[field]);
+      });
+    } else {
+      this.tableDataAll.reverse();
+    }
+  }
+
+  sortCreateDateArray(field) {
+    const cmp = this;
+    cmp.sortClicked = ! cmp.sortClicked;
+    if (!cmp.sortClicked) {
+      this.tableDataAll.sort( function(name1, name2) {
+        if ( Date.parse(name1[field]) < Date.parse(name2[field]) ) {
+          return -1;
+        } else if ( Date.parse(name1[field]) > Date.parse(name2[field])) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    } else {
+      this.tableDataAll.reverse();
+    }
   }
 
 }
