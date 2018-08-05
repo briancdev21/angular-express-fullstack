4cb033f2ba15ba2ecd5ba098f1016740715b7877
Latif <leadhead1987@gmail.com>
2018-08-05 22:53:43 +0200

    Update pending project scope tab

--- src/app/components/pm/pendings/pendingproject/pendingprojectscope/pendingprojectscope.component.ts
+++ src/app/components/pm/pendings/pendingproject/pendingprojectscope/pendingprojectscope.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PendingProjectService } from '../pendingproject.service';
+import { ProjectsService } from '../../../../../services/projects.service';
 
 @Component({
   selector: 'app-pendingprojectscope',
@@ -15,8 +16,15 @@ export class PendingProjectScopeComponent implements OnInit {
     workScope: '',
     internalNotes: ''
   };
+  projectInformation: any;
+  currentProjectId: any;
 
-  constructor( private router: Router, private pendingProjectService: PendingProjectService ) {
+  constructor( private router: Router, private pendingProjectService: PendingProjectService, private projectsService: ProjectsService ) {
+    this.currentProjectId = localStorage.getItem('current_pending_projectId');
+    this.projectsService.getIndividualProject(this.currentProjectId).subscribe(res => {
+      this.projectInformation = res.data;
+      console.log('projectInformation: ', this.projectInformation);
+    });
   }
 
   ngOnInit() {
@@ -28,7 +36,22 @@ export class PendingProjectScopeComponent implements OnInit {
   }
 
   toProjectTasks() {
-    this.router.navigate(['./pm/pending-project/pending-tasks']);
+    const savingData = {
+      'projectManager': this.projectInformation.projectManager,
+      'accountManager': this.projectInformation.accountManager,
+      'clientProjectManagerId': Number(this.projectInformation.clientProjectManagerId.split('-').pop()),
+      'accountReceivableId': Number(this.projectInformation.accountReceivableId.split('-').pop()),
+      'status': this.projectInformation.status,
+      'internalNote': this.projectInformation.internalNote,
+      'followers': this.projectInformation.followers
+    };
+    if (!savingData.internalNote) {
+      savingData.internalNote = '';
+    }
+    this.projectsService.updateIndividualProject(this.currentProjectId, savingData).subscribe(res => {
+      console.log('updated: ', res);
+      this.router.navigate(['./pm/pending-project/pending-tasks']);
+    });
   }
 
 }
