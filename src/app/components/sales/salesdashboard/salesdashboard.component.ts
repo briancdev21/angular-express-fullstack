15d5972002d1a5b088d5140cf2b5b6962b40b277
Latif <leadhead1987@gmail.com>
2018-08-11 22:39:36 +0200

    Complete sales dashboard page integration

--- src/app/components/sales/salesdashboard/salesdashboard.component.ts
+++ src/app/components/sales/salesdashboard/salesdashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectorRef, Input, OnInit, HostListener, EventEmitter, Output, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import * as _ from 'lodash';
+import { SharedService } from '../../../services/shared.service';
 
 
 @Component({
@@ -15,81 +16,27 @@ import * as _ from 'lodash';
 
 export class SalesDashboardComponent implements OnInit {
 
-  revenueTime = 'month';
-  donutTimePeriod = 'month';
+  revenueTime = 'MONTHLY';
+  donutTimePeriod = 'MONTHLY';
   menuCollapsed = true;
 
-  public revenueAreaChartInfo = [
-    {
-        period: 'JAN',
-        revenue: 30000,
-    }, {
-        period: 'FEB',
-        revenue: 25000,
-    }, {
-        period: 'MAR',
-        revenue: 20000,
-    }, {
-        period: 'APR',
-        revenue: 25000,
-    }, {
-        period: 'MAY',
-        revenue: 20000,
-    }, {
-        period: 'JUN',
-        revenue: 15000,
-    }, {
-        period: 'JUL',
-        revenue: 14000,
-    }, {
-        period: 'AUG',
-        revenue: 30000,
-    }, {
-        period: 'SEP',
-        revenue: 20000,
-    }, {
-        period: 'OCT',
-        revenue: 19000,
-    }, {
-        period: 'NOV',
-        revenue: 34000,
-    }, {
-        period: 'DEC',
-        revenue: 30000,
-    }];
+  public revenueAreaChartInfo: any;
 
   public invoicingInfo = {
-    openEstimate: 6,
-    openInvoice: 5,
-    proposalCount: 16
+    openEstimate: 0,
+    openInvoice: 0,
+    proposalsTotal: 0,
+    outstandingInvoices: 0
   };
 
   public proposalForecasting = {
-    newProposal: 6,
-    proposalInPipe: 10,
-    approvalTime: 61,
-    closed: 3
+    newProposal: 0,
+    proposalInPipe: 0,
+    approvalTime: 0,
+    closed: 0
   };
 
-  salesInfo = [
-    {
-      member: 'Michael Yue',
-      proposalsValue: 63622.32,
-      approvedValue: 23231,
-      totalMargin: 43
-    }, {
-      member: 'Michael Yue',
-      proposalsValue: 63622.32,
-      approvedValue: 23231,
-      totalMargin: 43
-    },
-    {
-      member: 'Michael Yue',
-      proposalsValue: 63622.32,
-      approvedValue: 23231,
-      totalMargin: 43
-    }
-  ];
+  salesInfo: any;
 
   activitiesInfo = [
     {
@@ -160,39 +107,82 @@ export class SalesDashboardComponent implements OnInit {
 
   public morrisDonutColors = ['#ffd97f', '#fab2c0', '#80dad8', '#a1abb8', '#38849B', '#6EB1DD', '#FF7E7E', '#F79E5D', '#6F7B83'];
 
-  public morrisDonutInfo = [
-    {
-      label: 'New',
-      value: 49,
-    }, {
-      label: 'Follow Up',
-      value: 35
-    }, {
-      label: 'Seen',
-      value: 29
-    }, {
-      label: 'Demo',
-      value: 12
-    }, {
-      label: 'Negotiation',
-      value: 8
-    }, {
-      label: 'Won',
-      value: 58
-    }, {
-      label: 'Lost',
-      value: 2
-    }
-  ];
+  public morrisDonutInfo: any;
 
-  ngOnInit() {
-    const arr = this.morrisDonutInfo.map( v => v.value);
-    let total = 0;
-    arr.forEach(element => {
-      total = total + element;
+  showDonutChart = true;
+  showAreaChart = true;
+
+  constructor(private sharedService: SharedService) {
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'openEstimates').subscribe(res => {
+      this.invoicingInfo.openEstimate = res.openEstimates;
     });
-    this.morrisDonutInfo.forEach(ele => {
-      ele.value = Math.floor(ele.value * 100 / total);
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'openInvoices').subscribe(res => {
+      this.invoicingInfo.openInvoice = res.openInvoices;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'outstandingInvoices').subscribe(res => {
+      this.invoicingInfo.outstandingInvoices = res.outstandingInvoices;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'proposalsTotal').subscribe(res => {
+      this.invoicingInfo.proposalsTotal = res.proposalsTotal;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'userProposalPerformance').subscribe(res => {
+      this.salesInfo = res.userProposalPerformance;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'newProposals').subscribe(res => {
+      this.proposalForecasting.newProposal = res.newProposals;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'proposalsInPipeline').subscribe(res => {
+      this.proposalForecasting.proposalInPipe = res.proposalsInPipeline;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'averageProposalApprovalTime').subscribe(res => {
+      this.proposalForecasting.approvalTime = res.averageProposalApprovalTime;
+    });
+
+    this.sharedService.getSalesStatistics(0, 0, 'MONTHLY', 'wonProposalCount').subscribe(res => {
+      this.proposalForecasting.closed = res.wonProposalCount;
+    });
+
+    this.fetchPipelineData('MONTHLY');
+
+    this.fetchRevenueData('MONTHLY');
+
+  }
+  ngOnInit() {
+  }
+
+  fetchPipelineData(unit) {
+    this.sharedService.getSalesStatistics(0, 0, unit, 'salesPipelineOverTime').subscribe(sales => {
+      this.morrisDonutInfo = sales.salesPipelineOverTime;
+      this.showDonutChart = false;
+      const total = this.morrisDonutInfo.map(h => h.value).reduce((a, b) => a + b, 0);
+      setTimeout(() => {
+        this.showDonutChart = true;
+      });
+      this.morrisDonutInfo.forEach(element => {
+        element.label = element.status;
+        element.value = (element.value / total * 100).toFixed(1);
+      });
+    });
+  }
+
+  fetchRevenueData(unit) {
+    this.sharedService.getSalesStatistics(11, 0, unit, 'revenueOverTime').subscribe(sales => {
+      this.revenueAreaChartInfo = sales.revenueOverTime;
+      this.showAreaChart = false;
+      setTimeout(() => {
+        this.showAreaChart = true;
+      });
+      this.revenueAreaChartInfo.forEach(element => {
+        element.revenue = element.frameValue;
+        element.period = element.frameUnit;
+      });
     });
   }
 }
