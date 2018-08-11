00e4b155ee20014dcd609f007db34661965f1844
Latif <leadhead1987@gmail.com>
2018-08-11 20:24:14 +0200

    Complete pm dashbaord integration

--- src/app/components/pm/pmdashboard/pmdashboard.component.ts
+++ src/app/components/pm/pmdashboard/pmdashboard.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import * as _ from 'lodash';
 import * as moment from 'moment';
 import { SharedService } from '../../../services/shared.service';
+import { ProjectsService } from '../../../services/projects.service';
 
 @Component({
   selector: 'app-pmdashboard',
@@ -24,107 +25,17 @@ export class PmDashboardComponent implements OnInit {
   };
 
   public projectsInfo = {
-    newProject: 3,
-    activeProject: 5,
-    completedproject: 2,
-    pendingProject: 2
+    newProject: 0,
+    activeProject: 0,
+    completedproject: 0,
+    pendingProject: 0
   };
 
-  public activeProjects = [
-    {
-      projectName: 'Project 1',
-      dueDate: '2018-02-12'
-    }, {
-      projectName: 'Project 1',
-      dueDate: '2018-03-12'
-    }, {
-      projectName: 'Project 1',
-      dueDate: '2018-03-28'
-    }, {
-      projectName: 'Project 1',
-      dueDate: '2018-04-12'
-    }, {
-      projectName: 'Project 1',
-      dueDate: '2018-05-30'
-    },
-  ];
+  public activeProjects: any;
 
-  public areaChartInfo = [
-    {
-        period: 'JAN',
-        Actual: 35000,
-        Estimate: 30000,
+  public areaChartInfo: any;
 
-    }, {
-        period: 'FEB',
-        Actual: 15000,
-        Estimate: 25000,
-
-    }, {
-        period: 'MAR',
-        Actual: 31000,
-        Estimate: 20000,
-
-    }, {
-        period: 'APR',
-        Actual: 25000,
-        Estimate: 25000,
-
-    }, {
-        period: 'MAY',
-        Actual: 18000,
-        Estimate: 20000,
-
-    }, {
-        period: 'JUN',
-        Actual: 17000,
-        Estimate: 15000,
-    }, {
-        period: 'JUL',
-        Actual: 20000,
-        Estimate: 14000,
-    }, {
-        period: 'AUG',
-        Actual: 32000,
-        Estimate: 30000,
-    }, {
-        period: 'SEP',
-        Actual: 23000,
-        Estimate: 20000,
-    }, {
-        period: 'OCT',
-        Actual: 32000,
-        Estimate: 19000,
-    }, {
-        period: 'NOV',
-        Actual: 20000,
-        Estimate: 34000,
-    }, {
-        period: 'DEC',
-        Actual: 32000,
-        Estimate: 30000,
-    }];
-
-  public morrisDonutInfo = [
-    {
-      label: '8802-0212',
-      value: 49,
-    }, {
-      label: '8802-0213',
-      value: 35
-    }, {
-      label: '8802-0214',
-      value: 8
-    }, {
-      label: '8802-0215',
-      value: 12
-    }, {
-      label: '8802-0216',
-      value: 8
-    }, {
-      label: 'other',
-      value: 99
-    }];
+  public morrisDonutInfo: any;
 
   public morrisDonutColors = ['#ffd97f', '#fab2c0', '#80dad8', '#a1abb8', '#38849B', '#6EB1DD', '#FF7E7E', '#F79E5D', '#6F7B83'];
 
@@ -198,9 +109,10 @@ export class PmDashboardComponent implements OnInit {
   ];
 
   menuCollapsed = true;
-  donutTimePeriod = 'month';
+  donutTimePeriod = 'MONTHLY';
+  showDonutChart = true;
 
-  constructor(private sharedService: SharedService) {
+  constructor(private sharedService: SharedService, private projectsService: ProjectsService) {
     this.sharedService.getProjectsStatistics(5, 0, 'MONTHLY', 'projectsRevenueOverTime').subscribe(res => {
       this.morrisLineChartInfo = res.projectsRevenueOverTime;
       this.morrisLineChartInfo.forEach(ele => {
@@ -224,7 +136,85 @@ export class PmDashboardComponent implements OnInit {
     this.sharedService.getProjectsStatistics(0, 0, 'MONTHLY', 'overDueProjectTasks').subscribe(res => {
       this.projectTasks.overDue = res.overDueProjectTasks;
     });
+
+    this.sharedService.getProjectsStatistics(0, 0, 'MONTHLY', 'newProjectsCount').subscribe(res => {
+      this.projectsInfo.newProject = res.newProjectsCount;
+    });
+
+    this.sharedService.getProjectsStatistics(0, 0, 'MONTHLY', 'activeProjectsCount').subscribe(res => {
+      this.projectsInfo.activeProject = res.activeProjectsCount;
+    });
+
+    this.sharedService.getProjectsStatistics(0, 0, 'MONTHLY', 'completedProjectsCountOverTime').subscribe(res => {
+      this.projectsInfo.completedproject = res.completedProjectsCountOverTime[0].frameValue;
+    });
+
+    this.sharedService.getProjectsStatistics(0, 0, 'MONTHLY', 'pendingProjectsCount').subscribe(res => {
+      this.projectsInfo.pendingProject = res.pendingProjectsCount;
+    });
+
+    this.sharedService.getProjectsStatistics(11, 0, 'MONTHLY', 'actualProjectBugdetOverTime').subscribe(res => {
+      const areaActualData = res.actualProjectBugdetOverTime;
+      this.sharedService.getProjectsStatistics(11, 0, 'MONTHLY', 'estimatedProjectBugdetOverTime').subscribe(est => {
+        const areaEstimatedData = est.estimatedProjectBugdetOverTime;
+        this.areaChartInfo = [];
+        for (let i = 0; i < 12; i ++) {
+          const addingIndi = {
+            Actual: areaActualData[i].frameValue,
+            Estimate: areaEstimatedData[i].frameValue,
+            period: areaEstimatedData[i].frameUnit,
+          };
+          this.areaChartInfo.push(addingIndi);
+        }
+      });
+    });
+
+    this.projectsService.getProjectsList().subscribe(res => {
+      const projectsList = res.results;
+      console.log('projectsList: ', res);
+      this.activeProjects = projectsList.filter(p => p.status === 'IN_PROGRESS');
+      this.activeProjects.forEach(element => {
+        element.dueDate = moment(element.endDate).format('MMMM DD, YYYY');
+      });
+
+      this.sortDateArray('dueDate');
+    });
+
+    this.fetchProjectHoursData('MONTHLY');
   }
+
+  fetchProjectHoursData(unit) {
+    this.sharedService.getProjectsStatistics(0, 0, unit, 'projectHoursOverTime').subscribe(sales => {
+      this.morrisDonutInfo = sales.projectHoursOverTime;
+      this.showDonutChart = false;
+      const total = this.morrisDonutInfo.map(h => h.value).reduce((a, b) => a + b, 0);
+      setTimeout(() => {
+        this.showDonutChart = true;
+      });
+      this.morrisDonutInfo.forEach(element => {
+        element.label = element.name;
+        element.value = (element.value / total * 100).toFixed(1);
+      });
+    });
+  }
+
+  donutTimeChange(unit) {
+    this.fetchProjectHoursData(unit);
+  }
+
+  sortDateArray(field) {
+    this.activeProjects.sort( function(name1, name2) {
+      if ( Date.parse(name1[field]) < Date.parse(name2[field]) ) {
+        return -1;
+      } else if ( Date.parse(name1[field]) > Date.parse(name2[field])) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    this.activeProjects.reverse();
+  }
+
   ngOnInit() {
   //   this.activeProjects.map( p => p.dueDate = moment(p.dueDate).format('MMMM DD, YYYY'));
   //   const arr = this.morrisDonutInfo.map( v => v.value);
@@ -235,5 +225,5 @@ export class PmDashboardComponent implements OnInit {
   //   this.morrisDonutInfo.forEach(ele => {
   //     ele.value = Math.floor(ele.value * 100 / total);
   //   });
-  // }
+  }
 }
