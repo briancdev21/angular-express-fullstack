fce5eb20d1edca5c8c0269e656b55d290e3075a0
webdeveloper <webdeveloper.ernit@gmail.com>
2018-08-16 23:08:44 +0200

    Update product profile lively

--- src/app/components/inventory/products/productprofile/productprofile.component.ts
+++ src/app/components/inventory/products/productprofile/productprofile.component.ts
@@ -78,6 +78,12 @@ export class ProductProfileComponent implements OnInit {
         this.retrieveVariantsData();
       }
     });
+
+    this.productProfileService.productDetailsUpdate.subscribe(res => {
+      if (res) {
+        this.retrieveVariantsData();
+      }
+    });
   }
 
   retrieveVariantsData() {
