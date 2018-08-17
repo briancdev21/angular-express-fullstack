5d0d05ac6132a5bbf89f713b13be73a17adb1438
Latif <leadhead1987@gmail.com>
2018-08-17 04:34:52 +0200

    Save data when clicking outside
    Save data when clicking outside of internal note in pend project progress page

--- src/app/components/pm/pendings/pendingproject/pendingprojectscope/pendingprojectscope.component.ts
+++ src/app/components/pm/pendings/pendingproject/pendingprojectscope/pendingprojectscope.component.ts
@@ -18,12 +18,14 @@ export class PendingProjectScopeComponent implements OnInit {
   };
   projectInformation: any;
   currentProjectId: any;
+  savedInternalNote: any;
 
   constructor( private router: Router, private pendingProjectService: PendingProjectService, private projectsService: ProjectsService ) {
     this.currentProjectId = localStorage.getItem('current_projectId');
     this.projectsService.getIndividualProject(this.currentProjectId).subscribe(res => {
       this.projectInformation = res.data;
       console.log('projectInformation: ', this.projectInformation);
+      this.savedInternalNote = this.projectInformation.internalNote;
     });
   }
 
@@ -36,25 +38,39 @@ export class PendingProjectScopeComponent implements OnInit {
   }
 
   toProjectTasks() {
-    const savingData = {
-      'projectManager': this.projectInformation.projectManager,
-      'accountManager': this.projectInformation.accountManager,
-      'clientProjectManagerId': this.projectInformation.clientProjectManagerId.split('-').pop(),
-      'accountReceivableId': this.projectInformation.accountReceivableId.split('-').pop(),
-      'status': this.projectInformation.status,
-      'internalNote': this.projectInformation.internalNote,
-      'followers': this.projectInformation.followers
-    };
-    if (!savingData.internalNote) {
-      savingData.internalNote = '';
+    this.router.navigate(['./pm/pending-project/pending-tasks']);
+  }
+
+  updateProjectInfo() {
+    if (this.savedInternalNote !== this.projectInformation.internalNote) {
+      const savingData = {
+        'projectManager': this.projectInformation.projectManager,
+        'accountManager': this.projectInformation.accountManager,
+        'clientProjectManagerId': this.projectInformation.clientProjectManagerId.split('-').pop(),
+        'accountReceivableId': this.projectInformation.accountReceivableId.split('-').pop(),
+        'status': this.projectInformation.status,
+        'internalNote': this.projectInformation.internalNote,
+        'followers': this.projectInformation.followers
+      };
+      if (!savingData.internalNote) {
+        savingData.internalNote = '';
+      }
+      if (!savingData.followers) {
+        savingData.followers = [];
+      }
+      this.projectsService.updateIndividualProject(this.currentProjectId, savingData).subscribe(res => {
+        console.log('updated: ', res);
+        this.savedInternalNote = res.data.internalNote;
+      });
     }
-    if (!savingData.followers) {
-      savingData.followers = [];
-    }
-    this.projectsService.updateIndividualProject(this.currentProjectId, savingData).subscribe(res => {
-      console.log('updated: ', res);
-      this.router.navigate(['./pm/pending-project/pending-tasks']);
-    });
+  }
+
+  editorChange(event) {
+    console.log('changed: ', event);
+  }
+
+  editorBlurred(event) {
+    console.log('blurred: ', event);
   }
 
 }
